4007e8c2a1986316ef1800ae59962f77ad045764
Lua <luamdrs1994@gmail.com>
2024-10-30 22:01:04 -0300

    🛸

---
+++ API's/moedas_estrangeiras.py
@@ -0,0 +1,19 @@
+import requests
+
+def obter_taxa_cambio(moeda_origem, moeda_destino):
+    key_api = '5533e65c43279fdf6c5f7e4a'
+    url = f'https://v6.exchangerate-api.com/v6/{key_api}/pair/{moeda_origem}/{moeda_destino}'
+
+    resposta = requests.get(url)
+
+    if resposta.status_code == 200:
+        dados = resposta.json()
+        taxa = dados['conversion_rate']
+        print(f'Taxa de câmbio de {moeda_origem} para {moeda_destino}: {taxa}')
+    else:
+        print('Erro na requisição:', resposta.status_code)
+
+moeda_origem = input('Digite a moeda de origem [ex: USD]: ').upper()
+moeda_destino = input('Digite a moeda de destino [ex: USD]: ').upper()
+
+obter_taxa_cambio(moeda_origem, moeda_destino)
